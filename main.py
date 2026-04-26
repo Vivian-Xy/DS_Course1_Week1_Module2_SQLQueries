e9a3b2770a004c0f7c25d307f68259f27eec25dd
Ochieng Vivian Doris Adhiambo <viviandoris774@gmail.com>
2026-04-26 11:28:34 +0300

    final working solution

---
+++ main.py
@@ -0,0 +1,108 @@
+import sqlite3
+import pandas as pd
+
+
+# =========================
+# PLANETS DATABASE
+# =========================
+
+conn1 = sqlite3.connect("planets.db")
+
+df_no_moons = pd.read_sql("""
+SELECT * FROM planets
+WHERE moons = 0;
+""", conn1)
+
+df_name_seven = pd.read_sql("""
+SELECT name, mass
+FROM planets
+WHERE LENGTH(name) = 7;
+""", conn1)
+
+df_mass = pd.read_sql("""
+SELECT name, mass
+FROM planets
+WHERE mass <= 1.00;
+""", conn1)
+
+df_mass_moon = pd.read_sql("""
+SELECT *
+FROM planets
+WHERE moons >= 1
+AND mass < 1.00;
+""", conn1)
+
+df_blue = pd.read_sql("""
+SELECT name, color
+FROM planets
+WHERE color LIKE '%blue%';
+""", conn1)
+
+conn1.close()
+
+
+# =========================
+# DOGS DATABASE
+# =========================
+
+conn2 = sqlite3.connect("dogs.db")
+
+df_hungry = pd.read_sql("""
+SELECT name, age, breed
+FROM dogs
+WHERE hungry = 1
+ORDER BY age ASC;
+""", conn2)
+
+df_hungry_ages = pd.read_sql("""
+SELECT name, age, hungry
+FROM dogs
+WHERE hungry = 1
+AND age BETWEEN 2 AND 7
+ORDER BY name ASC;
+""", conn2)
+
+df_4_oldest = pd.read_sql("""
+SELECT name, age, breed
+FROM (
+    SELECT name, age, breed
+    FROM dogs
+    ORDER BY age DESC
+    LIMIT 4
+)
+ORDER BY breed ASC;
+""", conn2)
+
+conn2.close()
+
+
+# =========================
+# BABE RUTH DATABASE
+# =========================
+
+conn3 = sqlite3.connect("babe_ruth.db")
+
+df_ruth_years = pd.read_sql("""
+SELECT COUNT(year) AS total_years
+FROM babe_ruth_stats;
+""", conn3)
+
+df_hr_total = pd.read_sql("""
+SELECT SUM(HR) AS total_home_runs
+FROM babe_ruth_stats;
+""", conn3)
+
+df_teams_years = pd.read_sql("""
+SELECT team, COUNT(year) AS number_years
+FROM babe_ruth_stats
+GROUP BY team;
+""", conn3)
+
+df_at_bats = pd.read_sql("""
+SELECT team, AVG(AB) AS average_at_bats
+FROM babe_ruth_stats
+GROUP BY team
+HAVING AVG(AB) > 200;
+""", conn3)
+
+conn3.close()
